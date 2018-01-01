ec521c7502328c0bad062c34c54a643080fc2c87
Amanda Dzierżanowska <dzierzanowska.amanda@gmail.com>
2018-01-01 18:11:55 +0100

    Change scripts.js content

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // scripts.js
 
-// var name = prompt('Enter your name');
-alert('Hello, ' + name);
+var name = prompt('Enter your name');
+// alert('Hello, ' + name);
 /* comment 1 */
 console.log('Hello, ' + name);
 // comment 2
